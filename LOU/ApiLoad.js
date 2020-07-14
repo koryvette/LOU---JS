03f2a9ef51a788fcda9869f3d780e910b73045a1
koryvette <korydhowell@yahoo.com>
2020-07-14 18:31:18 -0400

    updated index.js

--- LOU/ApiLoad.js
+++ LOU/ApiLoad.js
@@ -1,30 +1,6 @@
 const express = require ("express");
 const request = require('request');
 
-//Get current month to load into API url
-var today = new Date();
-var month = new Array();
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
-const currentMonth = month[today.getMonth()];
-
-//User input to capture API Key
-var userInput = window.prompt("Please enter API key:  ")
-var key = `${userInput}`;
-
-//Use API Key and Current month variables to create API
-const API = `http://api.eventful.com/rest/events/search?app_key=${key}&location=Louisville&date=${currentMonth}&page_size=250&page_number=1&within=25&units=miles`;
-
 //Make request to API to request data
 const LouEventDataRequest = (callback) =>{
     request(API, {xml: true}, (err, res, body) => {
@@ -85,4 +61,4 @@ function generateHtml(){
     Tile.innerHTML = html;
 }
 
-module.exports.callApi = LouEventDataRequest;
+//module.exports.callApi = LouEventDataRequest;
--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const app = express();
-const request = require('request');
-const LouEventDataRequest = require("./LOU")
+const request = require("request");
 
 const http = require('http');
 
-http.createServer((req, res) => {
-    if (req.API === "/request"){
-        LouEventDataRequest.callApi(function(response){
-            res.write(JSON.stringify(response));
-            res.end();
-        });
-    }
-    console.log ('listening at 3000')
-}).listen(3000);
 
+//Get current month to load into API url
+var today = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+const currentMonth = month[today.getMonth()];
 
+//User input to capture API Key
+var key = "NNQLDMQTJrwpbHSx";
+
+//Use API Key and Current month variables to create API
+const API = `http://api.eventful.com/rest/events/search?app_key=${key}&location=Louisville&date=${currentMonth}&page_size=250&page_number=1&within=25&units=miles`;
 
 app.use(express.static('LOU'));
 
+request(`${API}`, function(error, response, body){
+    if(error){
+        console.log("There was an error");
+    }else{
+        console.log(body);
+    }
+});
 
-// var key = "NNQLDMQTJrwpbHSx"
-
+app.listen(3100, function(){
+    console.log ("listening at 3000");
+});
 //cd source\repos\LOU_JS node index.js
